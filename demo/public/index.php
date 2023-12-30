582ddefdb68d4ae6cfcd077fe00aaa51a270a5bd
Dev-Tams <tammyalemu@gmail.com>
2023-12-30 01:34:21 +0100

    Updated my router to handles multiple requests

--- demo/public/index.php
+++ demo/public/index.php
@@ -1,24 +1,27 @@
 <?php
-
 const  BASE_PATH = __DIR__.'/../';
 
 
 
 spl_autoload_register(function ($class){
-      str_replace('\\','/', $class);
+    $class=  str_replace('\\','/', $class);
 
     require base_path($class .'.php');
 });
 require BASE_PATH.('Core/functions.php');
-//require base_path ('Database.php');  
-//require base_path('Response.php');
+
 require base_path('Core/router.php');
 
 
 
 
+$router = new \Core\Router();
+$routes = require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
+$router->route($uri, $method);
 
 //dd($users);
 
--- demo/routes.php
+++ demo/routes.php
@@ -1,17 +1,26 @@
 <?php
-return[
+// return[
     
-    '/' => 'controller/index.php',
+//     '/' => 'controller/index.php',
 
-    '/about' => 'controller/about.php',
+//     '/about' => 'controller/about.php',
 
-    '/contact' => 'controller/contact.php',
+//     '/contact' => 'controller/contact.php',
 
-    '/notes' => 'controller/notes/index.php',
+//     '/notes' => 'controller/notes/index.php',
 
-    '/note/create' => 'controller/notes/create.php',
+//     '/note/create' => 'controller/notes/create.php',
 
-    '/note' => 'controller/notes/show.php',
+//     '/note' => 'controller/notes/show.php',
     
 
-];
+// ];
+
+$router->get("/","controller/index.php");
+$router->get("/about","controller/about.php");
+$router->get("/contact","controller/contact.php");
+
+$router->get("/notes","controller/notes/index.php");
+$router->get("/note/create","controller/notes/index.php");
+$router->get("/note","controller/notes/show.php");
+
