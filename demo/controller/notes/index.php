f486616d1cbb6990981eb43c8da1f93c0b1804d0
Dev-Tams <tammyalemu@gmail.com>
2024-01-20 00:22:47 +0100

    removed access to notes, resolving db connection

--- demo/controller/notes/index.php
+++ demo/controller/notes/index.php
@@ -7,14 +7,14 @@ use Core\Database;
 
 
 
-$db = App::resolve(Database::class);
+// $db = App::resolve(Database::class);
 
 
-$query= "SELECT * from notes where user_id = 1";
-$notes = $db->query($query)->get();
+// $query= "SELECT * from notes where user_id = 1";
+// $notes = $db->query($query)->get();
 
- view('notes/index.php', [
-    'notes' => $notes
+view('notes/error.php', [
+     //'notes' => $notes
 ] );
 
 //require '../views/notes/index.php';
