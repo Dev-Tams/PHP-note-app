91a1b0496b60ff06057eb7d81dbb2ef2ef42625b
Dev-Tams <tammyalemu@gmail.com>
2024-02-14 01:20:34 +0100

    Used vendor for autoloading, this is super handy than building an spl autoload register

--- demo/public/index.php
+++ demo/public/index.php
@@ -2,15 +2,13 @@
 
 use Core\Session;
 
+
 const  BASE_PATH = __DIR__.'/../';
+
+require BASE_PATH. 'vendor/autoload.php';
 
 session_start();
 
-// spl_autoload_register(function ($class){
-//     $class=  str_replace('\\','/', $class);
-
-//     require base_path($class .'.php');
-// });
 
 require BASE_PATH.('Core/functions.php');
 
